fa3c2999c33c5663903bccfd646d76619ebaa1e7
xwash <xwashazy@126.com>
2019-08-05 17:30:39 +0800

    第一次更改

--- day01-code/src/cn/day01/demo01/HelloWord.java
+++ day01-code/src/cn/day01/demo01/HelloWord.java
@@ -2,6 +2,10 @@ package cn.day01.demo01;
 
 public class HelloWord {
     public static void main(String[] args) {
+
+        System.out.println("hello,word!");
+        System.out.println("hello,word!");
+        System.out.println("hello,word!");
         System.out.println("hello,word!");
     }
 
